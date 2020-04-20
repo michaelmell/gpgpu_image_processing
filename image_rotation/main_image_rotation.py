ffe247d539c0f87a8efad3ce8250f6bc9d119913
Michael Mell <michael.mell@gmx.net>
2020-04-20 11:56:37 +0200

    Simplify kernel call.

--- image_rotation/main_image_rotation.py
+++ image_rotation/main_image_rotation.py
@@ -11,7 +11,6 @@ Here we could use a variable sampler:
 def main():
     # prepare data
     imgIn = cv2.imread('photographer.png', cv2.IMREAD_GRAYSCALE)
-    imgIn = imgIn[128:512, :]
 
     rotation_angle = np.pi/4
     cos_theta = np.cos(rotation_angle)
@@ -29,28 +28,13 @@ def main():
     shape = imgIn.T.shape
     imgOut = np.empty_like(imgIn)
 
-    # (2) create image buffers which hold images for OpenCL
+    # create image buffers which hold images for OpenCL
     imgInBuf = cl.image_from_array(context, ary=imgIn, mode="r", norm_int=True, num_channels=1)
-    imgOutBuf = cl.Image(context, cl.mem_flags.WRITE_ONLY,
-                         cl.ImageFormat(cl.channel_order.LUMINANCE, cl.channel_type.UNORM_INT8),
-                         shape=shape)  # placeholder for gray-valued image of given shape
+    imgOutBuf = cl.image_from_array(context, ary=imgOut, mode="w", norm_int=True, num_channels=1)
 
-    # (3) load and compile OpenCL program
+    # load, compile and execute OpenCL program
     program = cl.Program(context, open('kernel.cl').read()).build()
-
-    # (3) from OpenCL program, get kernel object and set arguments (input image, operation type, output image)
-    kernel = cl.Kernel(program, 'img_rotate')  # name of function according to kernel.py
-    kernel.set_arg(0, imgInBuf)  # input image buffer
-    kernel.set_arg(1, imgOutBuf)  # output image buffer
-    kernel.set_arg(2, np.double(sin_theta))  # sinTheta
-    kernel.set_arg(3, np.double(cos_theta))  # cosTheta
-
-
-    # (4) copy image to device, execute kernel, copy data back
-    # cl.enqueue_copy(queue, imgInBuf, imgIn, origin=(0, 0), region=shape,
-    #                 is_blocking=False)  # copy image from CPU to GPU
-    cl.enqueue_nd_range_kernel(queue, kernel, shape,
-                               None)  # execute kernel, work is distributed across shape[0]*shape[1] work-items (one work-item per pixel of the image)
+    program.img_rotate(queue, shape, None, imgInBuf, imgOutBuf, np.double(sin_theta), np.double(cos_theta))
     cl.enqueue_copy(queue, imgOut, imgOutBuf, origin=(0, 0), region=shape,
                     is_blocking=True)  # wait until finished copying resulting image back from GPU to CPU
 
